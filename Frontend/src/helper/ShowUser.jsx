4c1bccd6afce4a09988bebde81e71a19be615144
jim2107054 <mdjahid205hasan@gmail.com>
2025-04-23 22:31:25 +0600

    .

--- Frontend/src/helper/ShowUser.jsx
+++ Frontend/src/helper/ShowUser.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const ShowUser = (props) => {
     const { nid, name, email, Phone, Division } = props;
   return (
-    <div className="w-4/5 flex md:w-64 md:px-0 md:gap-0 gap-5">
+    <div className="w-4/5 mt-5 flex md:w-64 md:px-0 md:gap-0 gap-5">
         <div className='h-fit w-full bg-[#716f6f2f] md:w-full flex flex-col rounded-lg py-2 px-5 gap-0.5'>
-        <h1 className='text-md font-sans'>NID: {nid}</h1>
-        <h1 className='text-md font-sans'>Name: {name}</h1>
-        <h1 className='text-sm font-sans'>Email: {email}</h1>
-        <h1 className='text-sm font-sans'>Phone: {Phone}</h1>
-        <h1 className='text-sm font-sans'>Address: {Division}</h1>
+        <h1 className='text-md font-sans text-[#f8d6d6] font-semibold'>NID: {nid}</h1>
+        <h1 className='text-md font-sans text-[white] font-medium'>Name: {name}</h1>
+        <h1 className='text-sm font-sans text-[white]'>Email: {email}</h1>
+        <h1 className='text-sm font-sans text-[white]'>Phone: {Phone}</h1>
+        <h1 className='text-sm font-sans text-[white]'>Address: {Division}</h1>
     </div>
     </div>
   )
